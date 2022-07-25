2ae4589f0a3f87fe46f5e2f9eca2589daf080fbc
Alex <alexmihgel@gmail.com>
2022-07-25 13:36:51 +0300

    add FireBase

--- wisdom/ViewController.swift
+++ wisdom/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseDatabase
 
 class ViewController: UIViewController {
 
+    let quotes = Quotes()
+    
+    @IBOutlet weak var labelQuotes: UILabel!
+    @IBOutlet weak var labelAuthor: UILabel!
+    @IBOutlet weak var textLabelQuotes: UITextView!
+    @IBOutlet weak var labelButtonNext: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        Database.database().reference().child("quotes").setValue(["quotestext" : "Что разум человека может постигнуть и во что он может поверить, того он способен достичь.", "author" : "Наполеон Хилл, журналист и писатель."])
+        updateTextLabel()
     }
 
-
+    @IBAction func buttonNextQuotes(_ sender: UIButton) {
+        updateTextLabel()
+    }
+    
+    @IBAction func gestureNextQuotes(_ sender: UISwipeGestureRecognizer) {
+        updateTextLabel()
+    }
+    
+    func updateTextLabel() {
+        textLabelQuotes.text = " \" \(quotes.quotesStorage.randomElement()![0]) \""
+        labelAuthor.text = quotes.quotesStorage.randomElement()![1]
+        textLabelQuotes.sizeToFit()
+    }
+    
+    func updateLabel() {
+        labelQuotes.text = quotes.quotesStorage.randomElement()![0]
+        labelAuthor.text = quotes.quotesStorage.randomElement()![1]
+    }
 }
 
